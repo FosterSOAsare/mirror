9d61b079796d1879a694332130f7c2d37c1a430f
FosterSOAsare <fostersoasare@gmail.com>
2023-10-27 17:39:53 +0000

    Adding social share previews

--- src/data/advices.ts
+++ src/data/advices.ts
@@ -1,10 +1,10 @@
 const advices = [
   {
     title: 'Visualize your ideal self and lifestyle',
-    advice: 'Understand that the ideal person is you and you alone. Your definition of self is what is right. Do not view yourself through the lens of others because you won’t get to see your real self.  '
+    advice: `Understand that the ideal person is you and you alone. Your definition of self is what is right. Do not view yourself through the lens of others because you won't get to see your real self.`
   }, {
-    title: 'Visualize your ideal self and lifestyle',
-    advice: 'Understand that the ideal person is you and you alone. Your definition of self is what is right. Do not view yourself through the lens of others because you won’t get to see your real self.  '
+    title: 'See and live the self',
+    advice: 'Believe in yourself, take on your challenges, dig deep within yourself to conquer fears. Never let anyone bring you down. You got to keep going. That is who you are.'
   }
 ]
 
